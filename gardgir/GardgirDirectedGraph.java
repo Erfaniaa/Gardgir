cdd4e552610b2ce1d765cae6db0be0e9778e40f5
Erfan Alimohammadi <erfan.aa@gmail.com>
2020-12-21 04:19:08 +0330

    Update GardgirDirectedGraph

--- gardgir/GardgirDirectedGraph.java
+++ gardgir/GardgirDirectedGraph.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 public class GardgirDirectedGraph {
 	public static final int NOT_FOUND_INT = -1;
 	public static final String NOT_FOUND_STRING = "";
+	public static final int NOT_MARKED = -1;
 	private int nodesCount;
 	private int edgesCount;
 	private ArrayList<String> nodeNames;
@@ -13,8 +14,9 @@ public class GardgirDirectedGraph {
 	private HashMap<String, Integer> stringToInteger;
 	private ArrayList<ArrayList<Integer> > adjacencyList;
 	private ArrayList<ArrayList<Integer> > reverseAdjacencyList;
-	private ArrayList<Boolean> dfsMark;
+	private ArrayList<Integer> dfsMark;
 	private ArrayList<Integer> dfsList;
+	private ArrayList<GardgirStronglyConnectedComponent> stronglyConnectedComponents;
 
 	public GardgirDirectedGraph() {
 		nodesCount = 0;
@@ -24,8 +26,9 @@ public class GardgirDirectedGraph {
 		nodeWeights = new ArrayList<Integer>();
 		adjacencyList = new ArrayList<ArrayList<Integer> >();
 		reverseAdjacencyList = new ArrayList<ArrayList<Integer> >();
-		dfsMark = new ArrayList<Boolean>();
+		dfsMark = new ArrayList<Integer>();
 		dfsList = new ArrayList<Integer>();
+		stronglyConnectedComponents = new ArrayList<GardgirStronglyConnectedComponent>();
 	}
 
 	public String getNodeName(int nodeIndex) {
@@ -51,7 +54,7 @@ public class GardgirDirectedGraph {
 			return stringToInteger.get(nodeName).intValue();
 		else {
 			nodesCount++;
-			dfsMark.add(false);
+			dfsMark.add(NOT_MARKED);
 			nodeNames.add(nodeName);
 			nodeWeights.add(Integer.valueOf(nodeWeight));
 			adjacencyList.add(new ArrayList<Integer>());
@@ -100,9 +103,9 @@ public class GardgirDirectedGraph {
 	public ArrayList<ArrayList<Integer> > findStronglyConnectedComponents() {
 		dfsList.clear();
 		for (int i = 0; i < nodesCount; i++)
-			dfsMark.set(i, false);
+			dfsMark.set(i, NOT_MARKED);
 		for (int i = 0; i < nodesCount; i++)
-			if (dfsMark.get(i) == false)
+			if (dfsMark.get(i) == NOT_MARKED)
 				dfs1(i);
 		for (int i = 0; i < nodesCount; i++)
 			dfsMark.set(i, false);
@@ -114,7 +117,7 @@ public class GardgirDirectedGraph {
 			}
 			System.out.println("______");
 		}
-		return null;
+		return stronglyConnectedComponents;
 	}
 
 	private void dfs1(int nodeIndex) {
@@ -134,5 +137,9 @@ public class GardgirDirectedGraph {
 			if (dfsMark.get(neighborNodeIndex.intValue()) == false)
 				dfs2(neighborNodeIndex, stronglyConnectedComponent);
 		}
+	}
+
+	public ArrayList<GardgirStronglyConnectedComponent> getSinkStronglyConnectedComponents() {
+		return null;
 	}
 }
